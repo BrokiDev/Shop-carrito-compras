8bb0c522e986d511535016902b5ac32355ff50bd
BrokiDev <bryantramirez855@gmail.com>
2023-06-19 22:22:13 -0400

    retoques finales al proyecto

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-const contenedorContenido = document.getElementById("contenedor-contenido");
+ const contenedorContenido = document.getElementById("contenedor-contenido");
 const modal = document.getElementById('ventana-modal');
 const carrito = document.getElementById('carrito');
 const closeButton = document.getElementsByClassName('close')[0];
@@ -8,13 +8,18 @@ const vaciarCarrito = document.getElementById('vaciar-carrito');
 const modalBody = document.querySelector('.modal-body');
 const eliminarProductoButton = document.querySelector('.eliminar-producto');
 const totalElement = document.getElementById('total');
+const iconMenu = document.getElementById('icon-menu');
 
 let productosCarrito = [];
 cargarEventos();
 
+
+//funcion para cargar los eventos
+
 function cargarEventos() {
-    document.addEventListener("DOMContentLoaded", cargarProductos);
-    contenedorContenido.addEventListener("click", agregarProducto);
+    iconMenu.addEventListener('click', showMenu)
+   document.addEventListener("DOMContentLoaded", cargarProductos);
+   contenedorContenido.addEventListener("click", agregarProducto);
     carrito.addEventListener("click", () => modal.style.display = 'block');
     closeButton.addEventListener("click", () => modal.style.display = 'none');
     productosCarrito = JSON.parse(localStorage.getItem('productoStorage')) || [];
@@ -23,33 +28,31 @@ function cargarEventos() {
     //Eventos agregados a estas constantes para finalizar compra o vacear el carrito  
   
 
-  finalizarCompra.addEventListener("click", () => {
-    eliminarProductoDelCarrito()
-    totalElement.innerText = "Total: $" + 0;
-    localStorage.clear()
-    Swal.fire(
-      'Listo!',
-      'Tu compra fue completada exitosamente!',
-      'success'
-    )
-});
+    finalizarCompra.addEventListener("click", () => {
+        eliminarProductoDelCarrito();
+        totalElement.innerText = "Total: $" + 0;
+        localStorage.clear();
+        Swal.fire(
+            'Listo!',
+            'Tu compra fue completada exitosamente!',
+            'success'
+        );
+    });
 
 
 
-vaciarCarrito.addEventListener("click", () => {
-  eliminarProductoDelCarrito()
-  totalElement.innerText = "Total: $" + 0;
-  localStorage.clear()
-})
+    vaciarCarrito.addEventListener("click", () => {
+        eliminarProductoDelCarrito();
+        totalElement.innerText = "Total: $" + 0;
+        localStorage.clear();
+    })
 }
 
 
+//Funcion que elimina un producto del modal
+
 function eliminarProducto(e){
     e.preventDefault();
-    // // Obtener el elemento del producto en el carrito y eliminarlo
-    // const producto = e.target.parentNode;
-    // producto.remove();
-
     if (e.target.classList.contains('eliminar-producto')) {
         const idProducto = parseInt(e.target.getAttribute('id'));
         productosCarrito = productosCarrito.filter((producto => producto.id !== idProducto))
@@ -60,6 +63,8 @@ function eliminarProducto(e){
 
 };
 
+//Funcion de agregar producto al carrito
+
 function agregarProducto(e) {
     e.preventDefault();
     if (e.target.classList.contains('agregar-carrito')) {
@@ -67,6 +72,9 @@ function agregarProducto(e) {
         datosProducto(productoAgregado);
     }
 }
+
+
+//Funcion que contiene los datos del producto
 
 function datosProducto(producto) {
     const datosProduct = {
@@ -82,6 +90,8 @@ function datosProducto(producto) {
 
     agregarAlCarrito(datosProduct);
 }
+
+//Funcion que agrega el producto al carrito
 
 function agregarAlCarrito(productoAgregar) {
     const carritoConArticulo = productosCarrito.some((producto) => producto.id === productoAgregar.id);
@@ -110,14 +120,16 @@ function agregarAlCarrito(productoAgregar) {
       })
 }
 
+//Funcion que muestra el producto en el modal
+
 function mostrarProductos() {
     eliminarProductoDelCarrito();
 
     productosCarrito.forEach((producto) => {
-        const { imagen, nombre, precio, cantidad, subtotal, id } = producto;
-        const div = document.createElement('div');
-        div.classList.add('contenedor-producto');
-        div.innerHTML = `
+         const { imagen, nombre, precio, cantidad, subtotal, id } = producto;
+         const div = document.createElement('div');
+         div.classList.add('contenedor-producto');
+         div.innerHTML = `
             <img src="${imagen}" width="100">
             <p>${nombre}</p>
             <p>${precio}</p>
@@ -125,11 +137,14 @@ function mostrarProductos() {
             <p>${subtotal}</p>
             <a href="#" class="eliminar-producto" id="${id}">X</a>
         `;
-        modalBody.appendChild(div);
-    });
+         modalBody.appendChild(div);
+     });
 
     calcularCarrito();
 }
+
+
+//Funcion que calcula el costo el carrito
 
 function calcularCarrito() {
     let total = productosCarrito.reduce((sumaTotal, producto) => sumaTotal + producto.subtotal, 0);
@@ -151,10 +166,13 @@ function guardarEnLocalStorage() {
 }
 
 
-//Funcion que carga los productos desde el archivo js
+//Funcion que carga los productos desde el archivo JSON localmente
 
-function cargarProductos () {
-    productos.forEach((producto) => {
+async function cargarProductos () {
+  await fetch("../data/productos.json")
+    .then((response) => response.json())
+    .then((data) => { 
+    data.forEach((producto) => {
     const divCreate = document.createElement('div');
     divCreate.classList.add('contenido-grid');
     divCreate.innerHTML += `
@@ -170,7 +188,19 @@ function cargarProductos () {
 
     contenedorContenido.appendChild(divCreate);
     }
-)}
+
+)})
+.catch((error) => error = alert("algo no anda bien con la ejecucion del sitio"))
+}
+
+function showMenu() {
+    let navbar = document.getElementById('myTopnav');
+    navbar.className = navbar.className === 'topnav' ? (navbar.className += ' responsive') : (navbar.className = 'topnav');
+}
+
+
+
+
 
 
 
